8165487ca03fc4c4fdb67c251bdf969d7c7d4b42
ssyeon <43790443+ssyeon-721@users.noreply.github.com>
2025-11-10 17:04:14 +0900

    Create deploy_container.sh

---
+++ scripts/deploy_container.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+AWS_REGION=ap-northeast-1
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+IMAGE_REPO=myapp
+
+echo ">>> Fetching latest image tag..."
+IMG_TAG=$(aws ecr describe-images \
+  --repository-name $IMAGE_REPO \
+  --region $AWS_REGION \
+  --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' \
+  --output text)
+
+echo ">>> Pulling latest image from ECR: $IMG_TAG"
+docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO:$IMG_TAG
+
+echo ">>> Stopping old container (if exists)"
+docker stop myapp || true
+
+echo ">>> Running new container"
+docker run -d --rm --name myapp -p 80:80 $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO:$IMG_TAG
